f1352f9bdc7bc3a839e935c920950295291969fe
Никита Зайцев <genius.zaytsev@gmail.com>
2023-08-17 18:21:54 +0500

    tests 2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY", default="string_from_.env")
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*", cast=Csv())
+ALLOWED_HOSTS = ['84.252.143.175', '127.0.0.1', 'localhost', 'foodgraminc.myftp.biz']
 
 CSRF_TRUSTED_ORIGINS = config(
     "CSRF_TRUSTED_ORIGINS",
